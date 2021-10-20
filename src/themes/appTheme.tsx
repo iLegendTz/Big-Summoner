9de15f854d7fa18e39597dc859506548e6757774
iLegendTz <miguel.isaac@live.com>
2021-10-20 17:23:33 -0500

    Se creo lightTheme.

--- src/themes/appTheme.tsx
+++ src/themes/appTheme.tsx
@@ -1,8 +1,27 @@
 import { StyleSheet } from "react-native";
 
+export const lightTheme = {
+    dark: false,
+    colors: {
+        primary: '#445fa5',
+        primaryVariant: '#3700B3',
+        secondary: '#03DAC6',
+        secondaryVariant: '#03DAC6',
+        background: '#FFFFFF',
+        surface: '#FFFFFF',
+        error: '#B00020',
+        onPrimary: '#FFFFFF',
+        onSecondary: '#FFFFFF',
+        onBackground: '#000000',
+        onSurface: '#000000',
+        onError: '#FFFFFF',
+    },
+}
+
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
         marginHorizontal: 20,
-    }
+    },
 });
+
